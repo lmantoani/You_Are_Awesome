3a4b9e23b9f9515b11c4bf6a611706cad9d8aa4f
Lucas Mantoani <lmantoani@zagmail.gonzaga.edu>
2022-01-14 15:44:12 -0800

    Initial Commit

---
+++ You_Are_Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You_Are_Awesome
+//
+//  Created by Lucas Mantoani on 1/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
